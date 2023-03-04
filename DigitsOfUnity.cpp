424b4735e341a26872072c6c207528028b92170c
Kadin Tang <75419216+muffin02@users.noreply.github.com>
2023-03-04 08:32:31 -0800

    Create DigitsOfUnity.cpp

---
+++ DigitsOfUnity.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int M = 998244353;
+typedef unsigned long long ull;
+typedef long long ll;
+ll modmul(ll a, ll b) {
+    ll ret = a * b - M * ll(1.L / M * a * b);
+    return ret + M * (ret < 0) - M * (ret >= (ll)M);
+}
+
+void test_case() {
+    int n, k, m; cin >> n >> k >> m;
+    vector<long long> b(m + 1);
+    function<int(int)> get_len = [&](int x) {
+        int res = 0;
+        while(x > 0) {
+            x >>= 1; ++res;
+        }
+        return res;
+    };
+
+    int z = get_len(m);
+    for(int i = 1; i <= m; ++i) {
+        vector<long long> ps(z + 1);
+        for(int j = 0; j < z; ++j) {
+            ps[j + 1] = ps[j] + !(i >> j & 1);
+        }
+        for(int j = z - 1; j >= 0; --j) {
+            if(i >> j & 1) {
+                if(!(m >> j & 1)) {
+                    break;
+                } else if(!j) {
+                    ++b[i];
+                }
+                continue;
+            }
+            if(m >> j & 1) {
+                b[i] += 1 << ps[j];
+            }
+            if(!j) {
+                ++b[i];
+            }
+        }
+    }
+
+    vector<long long> fact(m + 5, 1), ifact(m + 5, 1), r(m + 5, 1);
+    for(int i = 2; i <= m; ++i) {
+        fact[i] = fact[i - 1] * i % M;
+        r[i] = (M - (M / i) * r[M % i] % M) % M;
+        ifact[i] = ifact[i - 1] * r[i] % M;
+    }
+
+    long long res = 0;
+    for(int i = 0; i <= m; ++i) {
+        int p = __builtin_popcount(i) - k;
+        if(p >= 0 && b[i] - n >= 0) {
+            long long nCk = modmul(modmul(fact[p + k - 1], ifact[k - 1]), ifact[p]);
+            res = (res + modmul(modmul(fact[b[i]], ifact[b[i] - n]), nCk) * (p & 1 ? -1 : 1)) % M;
+            if(res < 0) {
+                res += M;
+            }
+        }
+    }
+    cout << res << '\n';
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    test_case();
+    return 0;
+}
